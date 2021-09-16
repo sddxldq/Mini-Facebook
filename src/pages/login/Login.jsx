d615bcc6b4436e5fc72949372af3b9fe3244b7a1
sddxldq <sddxldq@gmail.com>
2021-09-16 01:13:59 -0700

    Second Commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useRef, useContext } from "react";
 import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@material-ui/core";
 
 function Login() {
+	const email = useRef();
+	const password = useRef();
+	const { user, isFetching, dispatch } = useContext(AuthContext);
 	const handleClick = (e) => {
 		e.preventDefault();
+		loginCall(
+			{ email: email.current.value, password: password.current.value },
+			dispatch
+		);
 	};
+	console.log(user);
 
 	return (
 		<div className="login">
@@ -15,16 +26,31 @@ function Login() {
 				</div>
 				<div className="loginRight">
 					<form onSubmit={handleClick} className="loginBox">
-						<input placeholder="Email" type="email" className="loginInput" />
+						<input
+							placeholder="Email"
+							type="email"
+							required
+							ref={email}
+							className="loginInput"
+						/>
 						<input
 							placeholder="Password"
 							type="password"
+							required
+							ref={password}
+							minLength="6"
 							className="loginInput"
 						/>
-						<button className="loginButton">Log In</button>
+						<button className="loginButton" type="submit" disabled={isFetching}>
+							{isFetching ? <CircularProgress size="20px" /> : "Log In"}
+						</button>
 						<span className="loginForgot">Forgot Password?</span>
-						<button className="loginRegusterButton">
-							Create a new account
+						<button className="loginRegisterButton">
+							{isFetching ? (
+								<CircularProgress size="20px" />
+							) : (
+								"Create a New Account"
+							)}
 						</button>
 					</form>
 				</div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
+import { useHistory } from "react-router";
 import "./register.css";
 
 function Register() {
+	const username = useRef();
+	const email = useRef();
+	const password = useRef();
+	const passwordAgain = useRef();
+	const history = useHistory();
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+		if (passwordAgain.current.value !== password.current.value) {
+			passwordAgain.current.setCustomValidity("Passwords don't match!");
+		} else {
+			const user = {
+				username: username.current.value,
+				email: email.current.value,
+				password: password.current.value,
+			};
+			try {
+				await axios.post("/auth.register", user);
+				history.push("/login");
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	};
+
 	return (
 		<div className="login">
 			<div className="loginWrapper">
@@ -10,16 +37,43 @@ function Register() {
 					<span className="loginDesc">There is no secrets in SecretLand.</span>
 				</div>
 				<div className="loginRight">
-					<div className="loginBox">
-						<input placeholder="Username" className="loginInput" />
-						<input placeholder="Email" className="loginInput" />
-						<input placeholder="Password" className="loginInput" />
-						<input placeholder="Password Again" className="loginInput" />
-                        <button className="loginButton">Sign Up</button>
-                        <button className="loginRegusterButton">
-                            Log into you account
-                        </button>
-					</div>
+					<form onSubmit={handleClick} className="loginBox">
+						<input
+							placeholder="Username"
+							required
+							ref={username}
+							className="loginInput"
+						/>
+						<input
+							placeholder="Email"
+							type="email"
+							required
+							ref={email}
+							className="loginInput"
+						/>
+						<input
+							placeholder="Password"
+							type="password"
+							required
+							ref={password}
+							className="loginInput"
+							min="6"
+						/>
+						<input
+							placeholder="Password Again"
+							type="password"
+							required
+							ref={passwordAgain}
+							className="loginInput"
+							min="6"
+						/>
+						<button className="loginButton" type="submit">
+							Sign Up
+						</button>
+						<button className="loginRegusterButton">
+							Log into you account
+						</button>
+					</form>
 				</div>
 			</div>
 		</div>
